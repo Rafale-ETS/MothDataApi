bdf6895d92d430b1a10d6c166fb47134af5f091e
Christophe-A <christo-chibougamo@hotmail.com>
2022-11-14 16:09:57 -0500

    working basic api

--- main.py
+++ main.py
@@ -1,33 +1,79 @@
+from datetime import datetime
 from deta import Deta
-from flask import Flask
+from flask import Flask, request
 
-#TODO: Move this to it's own module/file that handles database access
-#       and data formatting.
 dta = Deta()
-dta_db = dta.Base("MothRaces")
+
+# This will get all items in the given db that match the query dict 
+# See Deta Base fetch for query behaviour.
+def get_all_items(db: dta.Base, query: dict = None) -> list[dict]:
+    res = db.fetch(query)
+    items = res.items
+
+    while res.last:
+        res = db.fetch(query, last=res.last)
+        items += res.items
+
+    return items
+
+dta_db_races = dta.Base("MothRaces")
+dta_db_data = dta.Base("MothData")
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET"])
-def base_route():
-    return "Unauthorized", 401
+#@app.route('/', methods=["GET"])
+#def base_route():
+#    return "Unauthorized", 401
 
-@app.route('/race', methods=["GET"])
+@app.route('/race', methods=["GET", "POST"])
+def race():
+    if request.method == "GET":
+        return list_races()
+    elif request.method == "POST":
+        return add_race()
+    else:
+        return "Unauthorised method", 401
+    
 def list_races():
-    return "TODO: return a list of the races"
+    # TODO: enable pagination of the results
+    return get_all_items(dta_db_races)
 
-@app.route('/race', methods=["POST"])
-def add_update_race():
-    return "TODO: add or update race in the database, return race_id."
+def add_race():
+    data = request.json
 
-@app.route('/race/<id>', methods=["GET"])
+    saved_at = datetime.utcnow().timestamp()
+    data["saved_at"] = saved_at
+
+    # TODO: do some sanity checks and format validation
+    ret_race = dta_db_races.insert(data)
+
+    return {"status": "success", "key": ret_race["key"], "msg": f"New race with id {ret_race['key']} added."}
+
+@app.route('/race/<id>', methods=["GET", "POST"])
+def race_id(id):
+    if request.method == "GET":
+        return list_datapoints(id)
+    elif request.method == "POST":
+        return add_datapoint(id)
+    else:
+        return "Unauthorised method", 401
+
 def list_datapoints(id):
-    return f"TODO: return a list of datapoints for this {id} race."
+    # TODO: enable pagination of the results
+    return get_all_items(dta_db_data)
 
-@app.route('/race/<id>', methods=["POST"])
 def add_datapoint(id):
-    return f"TODO: add a datapoint to the database for the race {id}."
+    data = request.json
+
+    data["race_id"] = id
+    saved_at = datetime.utcnow().timestamp()
+    data["saved_at"] = saved_at
+
+    # TODO: do some sanity checks and format validation
+    ret_data = dta_db_data.insert(data)
+
+    return {"status": "success", "key": ret_data["key"], "race_id": ret_data["race_id"], "msg": f"New data for race with id {ret_data['race_id']} added."}
 
 @app.route('/race/<id>/<data_id>', methods=["GET"])
 def get_datapoint(id, data_id):
-    return f"TODO: return the data for this specific race {id} and datapoint {data_id}."
+    return dta_db_data.get(data_id)
